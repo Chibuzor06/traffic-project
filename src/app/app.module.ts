9f99a07ddaeb79e837e5e13bb397c9acad84f1fa
Chibuzor Ezerumaka <cezerumaka@sysserve.com>
2018-08-29 10:55:52 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import {FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
+
+
+const routes: Routes = [
+  {path : '', component: SignInComponent},
+  {path : 'home', component: HomeComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomeComponent,
+    TopNavbarComponent,
+    SideNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
