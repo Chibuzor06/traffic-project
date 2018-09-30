6282bfbf4f06f806d7ffa3d364831900f6e885a7
Chibuzor Ezerumaka <cezerumaka@sysserve.com>
2018-09-30 18:58:06 +0100

    google map autocomplete functionality added

--- src/app/real-time-monitor/real-time-monitor.component.ts
+++ src/app/real-time-monitor/real-time-monitor.component.ts
@@ -15,16 +15,23 @@ export class RealTimeMonitorComponent implements OnInit, AfterViewInit {
   initMap = () => {
   const mapProp = {
     center: new google.maps.LatLng(6.5149, 3.38586),
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true
   };
   this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   const card = document.getElementById('pac-card');
   // let input = document.getElementById('pac-input');
   this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+  const options = {
+    types: ['geocode'],
+    componentRestrictions: {country: 'ng'},
+    fields: ['geometry', 'formatted_address']
+  };
 
-  this.autocomplete = new google.maps.places.Autocomplete(this.inputElement.nativeElement);
+  this.autocomplete = new google.maps.places.Autocomplete(this.inputElement.nativeElement, options);
   this.autocomplete.bindTo('bounds', this.map);
+  this.autocomplete.addListener('place_changed', this.getStreetCoords.bind(this));
 }
 
 
@@ -40,10 +47,11 @@ export class RealTimeMonitorComponent implements OnInit, AfterViewInit {
       this.initMap();
     } catch (e) {
       if (e instanceof ReferenceError) {
-        console.log('reference error catch');
+        // console.log('reference error catch');
         const DSLScript = document.createElement('script');
-        DSLScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDbv4M_18hTKH7mDohM8omkQAJxsdoJ6_M';
+        DSLScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDbv4M_18hTKH7mDohM8omkQAJxsdoJ6_M&libraries=places';
         DSLScript.type = 'text/javascript';
+        DSLScript.defer = true;
         document.body.appendChild(DSLScript);
         DSLScript.onload = this.initMap;
         document.body.removeChild(DSLScript);
@@ -51,5 +59,14 @@ export class RealTimeMonitorComponent implements OnInit, AfterViewInit {
     }
   }
 
+  getStreetCoords() {
+    const street = this.autocomplete.getPlace();
+    console.log(street);
+    if (street.geometry.viewport) {
+      this.map.fitBounds(street.geometry.viewport);
+    } else {
+      this.map.setCenter(street.geometry.location);
+    }
+  }
 
 }
